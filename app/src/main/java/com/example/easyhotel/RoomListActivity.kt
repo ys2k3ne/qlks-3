661d37e5834381b59572e0fcb151e795834a9de7
kimhuyen124 <huyenntk.21it@vku.udn.vn>
2023-04-27 00:25:23 +0700

    git commit -m"update new"

--- app/src/main/java/com/example/easyhotel/RoomListActivity.kt
+++ app/src/main/java/com/example/easyhotel/RoomListActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.SeekBar
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.easyhotel.Adapter.Room
@@ -19,19 +20,78 @@ import kotlin.collections.ArrayList
 private lateinit var binding: ActivityRoomListBinding
 
 class RoomListActivity : AppCompatActivity() {
-    private lateinit var ds: ArrayList<Room>
-    private lateinit var dbRef: DatabaseReference
     private lateinit var mAdapter: RoomAdapter
+    private var ds: MutableList<Room> = mutableListOf()
+    private lateinit var dbRef: DatabaseReference
+    private lateinit var query: Query
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRoomListBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val dbRef = FirebaseDatabase.getInstance().getReference("hotels")
+        query = dbRef.orderByChild("available").equalTo(true)
+
+// Khởi tạo mAdapter
+        mAdapter = RoomAdapter(ds, object : LSInterface {
+            override fun onClickRoom(pos: Int) {
+                val intent = Intent(this@RoomListActivity, DetailsActivity::class.java)
+                intent.putExtra("roomId", ds[pos].roomId)
+                startActivity(intent)
+
+                Toast.makeText(
+                    this@RoomListActivity,
+                    "Bạn chọn ${ds[pos].roomName}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        })
+        binding.filterTv.setOnClickListener {
+            // Lấy giá trị của bộ lọc
+            val hasAvailableRooms = binding.filterCheckbox.isChecked
+
+            // Nếu bộ lọc "khách sạn có sẵn phòng" được chọn, thêm điều kiện cho câu truy vấn
+            val filteredQuery = if (hasAvailableRooms) {
+                query
+            } else {
+                dbRef
+            }
+
+            // Thực hiện câu truy vấn và xử lý kết quả
+            filteredQuery.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    ds.clear()
+                    for (roomSnapshot in snapshot.children) {
+                        val room = roomSnapshot.getValue(Room::class.java)
+                        room?.let { ds.add(it) }
+                    }
+                    mAdapter.notifyDataSetChanged()
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Toast.makeText(this@RoomListActivity, "Error: ${error.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+
+
+
+
+
+        // Gán sự kiện cho view filterCheckBox
+        binding.filterCheckbox.setOnCheckedChangeListener { _, isChecked ->
+            // Xử lý khi check/uncheck filterCheckBox
+        }
+
+        // Gán sự kiện cho view priceSeekBar
+
+
 
         FirebaseApp.initializeApp(this)
         // Khởi tạo Adapter
-        binding.roomList.layoutManager = LinearLayoutManager(this)
-        binding.roomList.setHasFixedSize(true)
+        binding.hotelList.layoutManager = LinearLayoutManager(this)
+        binding.hotelList.setHasFixedSize(true)
 
         ds = ArrayList<Room>() // Khởi tạo biến ds với một danh sách rỗng
         GetThongTin()
@@ -99,7 +159,7 @@ class RoomListActivity : AppCompatActivity() {
 
                     })
                     // Gán adapter mới với RecyclerView
-                    binding.roomList.adapter = mAdapter
+                    binding.hotelList.adapter = mAdapter
                     // code lắng nghe sự kiện lên item rv
                     mAdapter.setOnItemClickListener(object : onItemClickListener {
                         override fun onItemClick(position: Int) {
